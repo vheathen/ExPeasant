47258aa11812a2a69c2ffa7fd96244e7cc3afb65
heathen <v.heathen@gmail.com>
2020-04-20 19:45:32 +0500

    Helper and child_spec

--- peasant/lib/peasant/helper.ex
+++ peasant/lib/peasant/helper.ex
@@ -3,6 +3,16 @@ defmodule Peasant.Helper do
   Various support functions
   """
 
-  @spec via_tuple(any) :: {:via, Registry, {PeasantRegistry, any}}
+  @spec via_tuple(id :: any) :: {:via, Registry, {PeasantRegistry, any}}
   def via_tuple(id), do: {:via, Registry, {PeasantRegistry, id}}
+
+  @spec handler_child_spec(handler :: atom(), params :: %{required(:uuid) => Ecto.UUID}) ::
+          Supervisor.child_spec()
+  def handler_child_spec(handler, %{uuid: uuid} = opts),
+    do: %{
+      id: uuid,
+      start: {handler, :start_link, [opts]},
+      restart: :transient,
+      type: :worker
+    }
 end
---
+++ peasant/test/peasant/helper_test.exs
@@ -0,0 +1,25 @@
+defmodule Peasant.HelperTest do
+  use ExUnit.Case
+
+  alias Peasant.Helper
+
+  test "via_tuple/1 should return a correct tuple" do
+    uuid = UUID.uuid4()
+
+    assert {:via, Registry, {PeasantRegistry, ^uuid}} = Helper.via_tuple(uuid)
+  end
+
+  test "child_spec/2 should return a correct child spec" do
+    uuid = UUID.uuid4()
+    handler = Peasant.Tool.Handler
+
+    state = %{uuid: uuid, other_params: "SomeValue"}
+
+    assert %{
+             id: ^uuid,
+             start: {^handler, :start_link, [^state]},
+             restart: :transient,
+             type: :worker
+           } = Helper.handler_child_spec(handler, state)
+  end
+end
