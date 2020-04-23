8801f99815ab53ecae562a4f5d22fb9c5199588d
heathen <v.heathen@gmail.com>
2020-04-23 18:45:28 +0500

    Peasant.Tool.Event: struct, new/1

---
+++ peasant/lib/peasant/tool/event.ex
@@ -0,0 +1,38 @@
+defmodule Peasant.Tool.Event do
+  alias Peasant.Tool.Action
+
+  @type t() :: struct()
+
+  defmacro __using__(_) do
+    quote do
+      alias unquote(__MODULE__)
+      alias Peasant.Tool.Action
+
+      defstruct [:action_ref, :tool_uuid, :details]
+
+      # @spec new(tool_uuid :: Ecto.UUID) ::
+      #         Event.t()
+      # def new(tool_uuid),
+      #   do: __new__(tool_uuid: tool_uuid)
+
+      # @spec new(action_ref :: Action.action_ref(), tool_uuid :: Ecto.UUID) ::
+      #         Event.t()
+      # def new(action_ref, tool_uuid),
+      #   do: __new__(action_ref: action_ref, tool_uuid: tool_uuid)
+
+      # @spec new(action_ref :: Action.action_ref(), tool_uuid :: Ecto.UUID, details :: term()) ::
+      #         Event.t()
+      # def new(action_ref, tool_uuid, event_details),
+      #   do: __new__(action_ref: action_ref, tool_uuid: tool_uuid, details: event_details)
+
+      # defoverridable(new: 3, new: 2, new: 1)
+
+      @spec new(params :: map() | keyword()) ::
+              Event.t()
+
+      def new(params), do: struct(__MODULE__, params)
+
+      defoverridable(new: 1)
+    end
+  end
+end
---
+++ peasant/test/peasant/tool/event_test.exs
@@ -0,0 +1,49 @@
+defmodule Peasant.Tool.EventTest do
+  use Peasant.GeneralCase
+
+  describe "Peasant.Tool.Event" do
+    @describetag :unit
+
+    defmodule FakeEvent2 do
+      use Peasant.Tool.Event
+    end
+
+    test "should allow to 'use' itself and define a new/1 function to create an event struct" do
+      ref = UUID.uuid4()
+      uuid = UUID.uuid4()
+      details = Faker.Lorem.sentences()
+      params = [action_ref: ref, tool_uuid: uuid, details: details]
+
+      assert %FakeEvent2{action_ref: ref, tool_uuid: uuid, details: details} =
+               FakeEvent2.new(params)
+
+      params = Enum.into(params, %{})
+
+      assert %FakeEvent2{action_ref: ref, tool_uuid: uuid, details: details} =
+               FakeEvent2.new(params)
+
+      assert %FakeEvent2{action_ref: ref, tool_uuid: uuid, details: nil} =
+               FakeEvent2.new(action_ref: ref, tool_uuid: uuid)
+
+      assert %FakeEvent2{action_ref: nil, tool_uuid: uuid, details: nil} =
+               FakeEvent2.new(tool_uuid: uuid)
+    end
+
+    defmodule FakeEvent3 do
+      use Peasant.Tool.Event
+
+      def new(params), do: struct(__MODULE__, params)
+    end
+
+    test "should allow override a new/3 function" do
+      ref = UUID.uuid4()
+      uuid = UUID.uuid4()
+      details = Faker.Lorem.sentences()
+
+      params = [action_ref: ref, tool_uuid: uuid, details: details]
+
+      assert %FakeEvent3{action_ref: ref, tool_uuid: uuid, details: details} =
+               FakeEvent3.new(params)
+    end
+  end
+end
