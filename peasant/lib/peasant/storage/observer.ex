04a8c0bf75cf3080d50bf29437ecad3e89302130
heathen <v.heathen@gmail.com>
2020-04-24 13:51:00 +0500

    Storage.Observer: now loads to memory and populates persisted tools on start

--- peasant/lib/peasant/storage/observer.ex
+++ peasant/lib/peasant/storage/observer.ex
@@ -8,6 +8,16 @@ defmodule Peasant.Storage.Observer do
     Attached
   }
 
+  @tools "tools"
+
+  @default_state %{
+    @tools => %{}
+  }
+
+  def list(domain), do: GenServer.call(__MODULE__, {:list, domain})
+
+  def clear, do: GenServer.call(__MODULE__, :clear)
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
@@ -15,32 +25,81 @@ defmodule Peasant.Storage.Observer do
   def init(_) do
     Peasant.subscribe("tools")
 
-    {:ok, %{}}
+    {:ok, @default_state, {:continue, :load}}
   end
 
-  def handle_info(%Registered{details: %{tool: tool}}, objects) do
+  def handle_continue(:load, objects) do
     objects =
-      case Map.get(objects, tool.uuid) do
+      Repo.get_all()
+      |> Enum.reduce(objects, fn
+        {_key, {domain, record}}, objs ->
+          update_in(objs, [domain, record.uuid], fn _ -> record end)
+
+        _, objs ->
+          objs
+      end)
+
+    {:noreply, objects, {:continue, :populate}}
+  end
+
+  def handle_continue(:populate, objects) do
+    objects
+    |> Enum.to_list()
+    |> Enum.reduce([], fn
+      {domain, records}, flat_list ->
+        Enum.reduce(
+          records,
+          flat_list,
+          fn {_, record}, list -> [{domain, record} | list] end
+        )
+
+      _, flat_list ->
+        flat_list
+    end)
+    |> Enum.each(fn
+      {@tools, %{uuid: _} = record} ->
+        Peasant.Tool.Handler.register(record)
+
+      some ->
+        require Logger
+        Logger.warn(inspect(some))
+        :ok
+    end)
+
+    {:noreply, objects}
+  end
+
+  def handle_call({:list, domain}, _from, objects) do
+    {:reply, objects[domain] |> Map.values(), objects}
+  end
+
+  def handle_call(:clear, _from, _objects) do
+    {:reply, :ok, @default_state}
+  end
+
+  def handle_info(%Registered{details: %{tool: tool}}, %{@tools => tools} = objects) do
+    objects =
+      case Map.get(tools, tool.uuid) do
         ^tool ->
           objects
 
         _ ->
-          tool = Repo.persist(tool, "tools")
-          Map.put(objects, tool.uuid, tool)
+          tool = Repo.persist(tool, @tools)
+          update_in(objects, [@tools, tool.uuid], fn _ -> tool end)
       end
 
     {:noreply, objects}
   end
 
-  def handle_info(%Attached{tool_uuid: uuid}, objects) do
+  def handle_info(%Attached{tool_uuid: uuid}, %{@tools => tools} = objects) do
     objects =
-      case Map.get(objects, uuid) do
+      case Map.get(tools, uuid) do
         %{attached: true} ->
           objects
 
         %{attached: false} = tool ->
-          tool = Repo.persist(%{tool | attached: true}, "tools")
-          Map.put(objects, tool.uuid, tool)
+          tool = Repo.persist(%{tool | attached: true}, @tools)
+          update_in(objects, [@tools, uuid], fn _ -> tool end)
       end
 
     {:noreply, objects}
--- peasant/test/peasant/storage/observer_test.exs
+++ peasant/test/peasant/storage/observer_test.exs
@@ -3,7 +3,20 @@ defmodule Peasant.Storage.ObserverTest do
 
   alias Peasant.Repo
 
+  alias Peasant.Tools.FakeTool
+
+  alias Peasant.Tool.Event.{
+    Registered,
+    Attached
+  }
+
+  alias Peasant.Storage.Observer
+
   @observer Peasant.Storage.Observer
+
+  setup do
+    [db: Repo.db()]
+  end
 
   describe "Observer" do
     @describetag :integration
@@ -16,28 +29,7 @@ defmodule Peasant.Storage.ObserverTest do
   describe "on Registered event" do
     @describetag :integration
 
-    alias Peasant.Tools.FakeTool
-
-    alias Peasant.Tool.Event.{
-      Registered,
-      Attached
-    }
-
-    setup do
-      %type{uuid: uuid} = tool = new_tool() |> FakeTool.new()
-      event = Registered.new(tool_uuid: tool.uuid, details: %{tool: tool})
-
-      db = Repo.db()
-
-      assert is_nil(CubDB.get(db, uuid))
-      assert is_nil(CubDB.get(db, {"tools", type, uuid}))
-
-      notify(event)
-
-      Process.sleep(100)
-
-      [tool: tool, db: db]
-    end
+    setup :registered_setup
 
     test "should persist a tool record on Registered event", %{
       tool: %type{uuid: uuid},
@@ -46,20 +38,97 @@ defmodule Peasant.Storage.ObserverTest do
       assert {"tools", %^type{uuid: ^uuid}} = CubDB.get(db, uuid)
       assert true == CubDB.get(db, {"tools", type, uuid})
     end
+  end
+
+  describe "on Attached event" do
+    @describetag :integration
+
+    setup [:registered_setup, :attached_setup]
 
     test "should persist a tool record on Attached event", %{
       tool: %{uuid: uuid},
       db: db
     } do
-      event = Attached.new(tool_uuid: uuid)
-
-      notify(event)
-
-      Process.sleep(100)
-
       assert {"tools", %{uuid: ^uuid, attached: true}} = CubDB.get(db, uuid)
     end
   end
 
+  describe "list(domain) function" do
+    @describetag :integration
+
+    setup :list_setup
+
+    test "should list all objects for the specified domain", %{list: list} do
+      assert list == Observer.list("tools") |> nilify_timestamps() |> Enum.sort()
+    end
+  end
+
+  describe "clear/0" do
+    @describetag :integration
+
+    test "should clear all records" do
+      :ok = Observer.clear()
+      assert [] == Observer.list("tools")
+    end
+  end
+
+  describe "on start" do
+    @describetag :integration
+
+    setup :list_setup
+
+    test "it should load all records", %{list: list} do
+      assert list == Observer.list("tools") |> nilify_timestamps() |> Enum.sort()
+    end
+
+    test "it should populate all records", %{list: list} do
+      Enum.each(list, fn %{uuid: uuid} ->
+        assert [{pid, nil}] = Registry.lookup(Peasant.Registry, uuid)
+        assert is_pid(pid)
+      end)
+    end
+  end
+
+  def registered_setup(%{db: db}) do
+    %type{uuid: uuid} = tool = new_tool() |> FakeTool.new() |> Map.put(:new, false)
+    event = Registered.new(tool_uuid: tool.uuid, details: %{tool: tool})
+
+    assert is_nil(CubDB.get(db, uuid))
+    assert is_nil(CubDB.get(db, {"tools", type, uuid}))
+
+    notify(event)
+
+    Process.sleep(100)
+
+    [tool: tool]
+  end
+
+  def attached_setup(%{tool: %{uuid: uuid}}) do
+    event = Attached.new(tool_uuid: uuid)
+
+    notify(event)
+    Process.sleep(100)
+
+    :ok
+  end
+
+  def list_setup(context) do
+    [tool: tool1] = registered_setup(context)
+    [tool: tool2] = registered_setup(context)
+
+    list = [tool1, tool2] |> Enum.sort()
+
+    assert list == Observer.list("tools") |> nilify_timestamps() |> Enum.sort()
+    GenServer.stop(Observer)
+    Process.sleep(100)
+
+    [list: list]
+  end
+
   defp notify(event), do: Peasant.broadcast("tools", event)
+
+  defp nilify_timestamps(records) when is_list(records),
+    do: Enum.map(records, &nilify_timestamps/1)
+
+  defp nilify_timestamps(record), do: %{record | inserted_at: nil, updated_at: nil}
 end
