6b3d13997e0bdb60e91eec60401246d9ac0a3f32
heathen <v.heathen@gmail.com>
2020-04-28 23:46:55 +0500

    Web: - added bootstrap etc to dependencies; - copied webpack and scss from liveview_dashboard; - Lato 2.0 font added.

--- peasant/assets/webpack.config.js
+++ peasant/assets/webpack.config.js
@@ -44,7 +44,30 @@ module.exports = (env, options) => {
               }
             }
           ]
-        }
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }, {
+            loader: 'sass-loader'
+          }]
+        },
+        {
+          test: /\.(woff2)$/,
+          loader: 'url-loader'
+        },
       ]
     },
     plugins: [
