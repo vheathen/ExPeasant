9f8cee3e128dcdd7cf6a911eb20f35d3a277ca4f
heathen <v.heathen@gmail.com>
2020-05-10 03:00:43 +0500

    Bootstrap + FontAwesome deps

--- peasant/assets/webpack.config.js
+++ peasant/assets/webpack.config.js
@@ -65,8 +65,14 @@ module.exports = (env, options) => {
           }]
         },
         {
-          test: /\.(woff2)$/,
-          loader: 'url-loader'
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts'
+            }
+          }]
         },
       ]
     },
