5f5af9169fdb0d64100bec413aed6148465e0154
heathen <v.heathen@gmail.com>
2020-05-12 14:42:07 +0500

    Tools: - tool_types/0; - get_actions_by_tool_type/1; - get_actions_by_tool/1.

--- peasant/lib/peasant/tools.ex
+++ peasant/lib/peasant/tools.ex
@@ -6,8 +6,17 @@ defmodule Peasant.Tools do
 
   @tools Peasant.Tool.domain()
 
-  def actions do
-    Repo.list_full(@action_to_tools)
+  def actions, do: Repo.list_full(@action_to_tools)
+
+  def tool_types, do: Repo.list_full(@tool_to_actions)
+
+  def get_actions_by_tool_type(tool_type), do: Repo.get(tool_type, @tool_to_actions)
+
+  def get_actions_by_tool(tool_uuid) do
+    tool_uuid
+    |> Repo.get(@tools)
+    |> Map.get(:__struct__)
+    |> Repo.get(@tool_to_actions)
   end
 
   def list do
--- peasant/test/peasant/tools_test.exs
+++ peasant/test/peasant/tools_test.exs
@@ -14,28 +14,54 @@ defmodule Peasant.ToolsTest do
   describe "Tools" do
     @describetag :unit
 
+    setup do
+      :sys.get_state(Peasant.Collection.Observer)
+
+      {:ok, tool_uuid} = Peasant.Tool.register(FakeTool, new_tool())
+      assert_receive %Peasant.Tool.Event.Registered{}
+
+      [tool_uuid: tool_uuid, tool_type: FakeTool]
+    end
+
     test "should have actions/0 func" do
       assert %{} = actions = Tools.actions()
 
-      for {action, tools} <- actions do
+      assert length(Map.keys(actions)) > 0
+
+      for {action, tool_types} <- actions do
         assert Code.ensure_compiled(action)
-        assert is_list(tools)
+        assert is_list(tool_types)
+        for type <- tool_types, do: assert(Code.ensure_compiled(type))
       end
     end
 
-    test "should have list/0" do
-      {:ok, tool_uuid} = Peasant.Tool.register(FakeTool, new_tool())
+    test "should have tool_types/0 func" do
+      assert %{} = tool_types = Tools.tool_types()
 
-      assert_receive %Peasant.Tool.Event.Registered{}
+      assert length(Map.keys(tool_types)) > 0
 
+      for {tool_type, actions} <- tool_types do
+        assert Code.ensure_compiled(tool_type)
+        assert is_list(actions)
+        for action <- actions, do: assert(Code.ensure_compiled(action))
+      end
+    end
+
+    test "should have get_actions_by_tool_type/1 func" do
+      for {tool_type, actions} <- Tools.tool_types() do
+        assert actions == Tools.get_actions_by_tool_type(tool_type)
+      end
+    end
+
+    test "should have get_actions_by_tool/1 func", %{tool_uuid: tool_uuid, tool_type: tool_type} do
+      assert Tools.get_actions_by_tool_type(tool_type) == Tools.get_actions_by_tool(tool_uuid)
+    end
+
+    test "should have list/0", %{tool_uuid: tool_uuid} do
       assert [%FakeTool{uuid: ^tool_uuid}] = Tools.list()
     end
 
-    test "should have get/1" do
-      {:ok, tool_uuid} = Peasant.Tool.register(FakeTool, new_tool())
-
-      assert_receive %Peasant.Tool.Event.Registered{}
-
+    test "should have get/1", %{tool_uuid: tool_uuid} do
       assert %FakeTool{uuid: ^tool_uuid} = Tools.get(tool_uuid)
     end
   end
