f357129b0c0cf17e205560db63ffa6bb426a5c2b
heathen <v.heathen@gmail.com>
2020-05-12 16:22:16 +0500

    Bump firmware version to 0.3.0: automation ready for test deploy

--- peasant/mix.exs
+++ peasant/mix.exs
@@ -4,7 +4,7 @@ defmodule Peasant.MixProject do
   def project do
     [
       app: :peasant,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
