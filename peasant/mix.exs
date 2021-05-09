2bf379f2127a0496f91f4e4f8c5df67225fee878
heathen <v.heathen@gmail.com>
2021-05-09 21:01:52 +0500

    Close #5: Phoenix upgraded to 1.5.8

--- peasant/mix.exs
+++ peasant/mix.exs
@@ -33,13 +33,13 @@ defmodule Peasant.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.4", override: true},
-      {:phoenix_live_view, "~> 0.14.0"},
-      {:floki, ">= 0.0.0", only: :test},
+      {:phoenix, "~> 1.5.8", override: true},
+      {:phoenix_live_view, "~> 0.15.1"},
+      {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.14"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.7"},
-      {:telemetry_metrics, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
@@ -51,7 +51,7 @@ defmodule Peasant.MixProject do
       {:ecto, "~> 3.4.0"},
       {:elixir_uuid, "~> 1.2"},
       {:cubdb, "~> 1.0.0-rc.4"},
-      {:cachex, "~> 3.2.0"},
+      {:cachex, "~> 3.3.0"},
       {:circuits_gpio, "~> 0.4"},
 
       # dev\test deps
